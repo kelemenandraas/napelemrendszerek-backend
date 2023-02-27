5e6a65c7aa944ee5ebc87bb1be302925f6f81022
Bandi <kelemenandraas0219@gmail.com>
2023-02-27 17:30:27 +0100

    ✨ add new model class

---
+++ napelemrendszerek-backend/Models/ProjectHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace napelemrendszerek_backend.Models
+{
+    class ProjectHistory
+    {
+        //attributes
+        public DateTime date { get; set; }
+
+        //connection
+        public int oldProjectStateID { get; set; }
+
+        public int newProjectStateID { get;set; }
+    }
+}
