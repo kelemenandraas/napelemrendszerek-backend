c9d2f6af09f7546e70e60c408de68d4be4b3227a
Bandi <kelemenandraas0219@gmail.com>
2023-02-27 15:09:08 +0100

    ✨ add EF models

---
+++ napelemrendszerek-backend/Models/ProjectStates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.ComponentModel.DataAnnotations;
+namespace napelemrendszerek_backend.Models
+{
+    class ProjectStates
+    {
+        //attributes
+        [Key]
+        public int projectStateID { get; set; }
+
+        [MaxLength(10)]
+        public string projectStateName { get; set; }
+
+        //connection
+        public ICollection<Project> Projects { get; set; }
+    }
+}
