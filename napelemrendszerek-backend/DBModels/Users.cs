b603c809fa391a730bc7cfd98a22aff813c6156a
Bandi <kelemenandraas0219@gmail.com>
2023-03-02 08:21:34 +0100

    🗃️ add new model classes

---
+++ napelemrendszerek-backend/DBModels/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace napelemrendszerek_backend.DBModels
+{
+    public partial class Users
+    {
+        public Users()
+        {
+            Project = new HashSet<Project>();
+        }
+
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string UserPassword { get; set; }
+        public int RoleId { get; set; }
+
+        public virtual Roles Role { get; set; }
+        public virtual ICollection<Project> Project { get; set; }
+    }
+}
