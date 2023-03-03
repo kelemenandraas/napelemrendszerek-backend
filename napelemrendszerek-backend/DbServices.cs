2915dd76ff6ae47721ed36d529744f53089a6bf0
Bandi <kelemenandraas0219@gmail.com>
2023-03-03 08:24:39 +0100

    🗃️ rename modified class names

--- napelemrendszerek-backend/DbServices.cs
+++ napelemrendszerek-backend/DbServices.cs
@@ -1,50 +1,53 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using SolarPanelSystem_backend.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using napelemrendszerek_backend.Models;
+using napelemrendszerek_backend.DBModels;
 
 namespace napelemrendszerek_backend
 {
     class DbServices
     {
-        SolarPanelContext SPContext = new SolarPanelContext();
+        SolarDBContext SPContext = new SolarDBContext();
         public DbServices()
         {
-            SPContext.Parts.Load();
+            SPContext.Part.Load();
             SPContext.Users.Load();
             SPContext.Roles.Load();
-            SPContext.Compartments.Load();
-            SPContext.Projects.Load();
+            SPContext.Compartment.Load();
+            SPContext.Project.Load();
             SPContext.ProjectStates.Load();
-            SPContext.PartProjectConnections.Load();
+            SPContext.PartProjectConnection.Load();
             SPContext.PartStates.Load();
         }
 
         public List<Project>  getAllProjects() {
             //TODO kiszűrni az adott oszlopokat
-            return SPContext.Projects.ToList();
+            return SPContext.Project.ToList();
         }
 
         public List<Project> getSingleProject(int projectID) {
 
-            return SPContext.Projects.Where(p => p.projectID == projectID).ToList();
+            return SPContext.Project.Where(p => p.ProjectId == projectID).ToList();
         }
 
         public List<Part> getParts() {
             //TODO kiszűrni a db/dobozt
-            return SPContext.Parts.ToList();
+            return SPContext.Part.ToList();
         }
 
         public void changeProjectState(int projectID,int stateID ) {
 
-            var selectedProject = SPContext.Projects.Where(p => p.projectID == projectID).Single();
-            selectedProject.projectStateID = stateID;
+            var selectedProject = SPContext.Project.Where(p => p.ProjectId == projectID).Single();
+            selectedProject.ProjectStateId = stateID;
 
             SPContext.SaveChanges();
+        }
+        public void teszt() {
+            var a = SPContext.Part.Where(p => p.PartId == 1).Single();
+            Console.WriteLine(a.PartId);
         }
 
         public void addUser() { 
